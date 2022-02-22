d1432112e0e93df4bd6d260e459a5781bb7e7d33
djazairi85 <mohamed.chohra@gmail.com>
2022-02-22 22:46:42 +0100

    added missing files

---
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/YahooFinanceDividendFeed.java
@@ -0,0 +1,67 @@
+package name.abuchen.portfolio.online.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.osgi.framework.FrameworkUtil;
+
+import com.google.common.base.Strings;
+
+import name.abuchen.portfolio.model.Security;
+import name.abuchen.portfolio.model.SecurityEvent.DividendEvent;
+import name.abuchen.portfolio.money.Money;
+import name.abuchen.portfolio.money.Values;
+import name.abuchen.portfolio.online.DividendFeed;
+import name.abuchen.portfolio.util.WebAccess;
+
+public class YahooFinanceDividendFeed implements DividendFeed
+{
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<DividendEvent> getDividendPayments(Security security) throws IOException
+    {   
+        long fromDate = LocalDateTime.now().minusYears(2).toEpochSecond(ZoneOffset.UTC);
+        long toDate = LocalDateTime.now().plusYears(2).toEpochSecond(ZoneOffset.UTC);
+        List<DividendEvent> answer = new ArrayList<>();
+        
+        String st = "https://query1.finance.yahoo.com/v7/finance/download/"+ security.getTickerSymbol() + "?period1=" + fromDate + "&period2=" + toDate + "&interval=1d&events=div&includeAdjustedClose=true";
+        URL stockURL = new URL(st);
+        BufferedReader in = new BufferedReader(new InputStreamReader(stockURL.openStream()));
+        String s = null;
+        while ((s=in.readLine())!=null) 
+        {
+            System.out.println(s);
+            if(!s.contains("Date"))
+            {
+               String[] tab = s.split(",");
+               LocalDate exDate = YahooHelper.fromISODate(tab[0]);
+               Double valuePerShare = Double.parseDouble(tab[1]);
+               DividendEvent payment = new DividendEvent();
+
+               payment.setDate(exDate); //$NON-NLS-1$
+               payment.setPaymentDate(exDate); //$NON-NLS-1$
+               
+               payment.setAmount(Money.of(security.getCurrencyCode(), //$NON-NLS-1$
+                               Values.Amount.factorize(valuePerShare))); //$NON-NLS-1$
+
+               payment.setSource("yahooFinance.com"); //$NON-NLS-1$
+               answer.add(payment);
+            }
+        }
+        return answer;
+    }
+
+}
