d7db3121f709dd7de0e1c66a3a90a66ef1c709cf
Mohamed Chohra <mohamed.chohra@gmail.com>
2023-04-08 15:34:41 +0200

    Take into account country for taxes

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/jobs/UpdateDividendsJob.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/jobs/UpdateDividendsJob.java
@@ -1,5 +1,6 @@
 package name.abuchen.portfolio.ui.jobs;
 
+import java.io.Console;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,6 +20,7 @@ import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.Security;
 import name.abuchen.portfolio.model.SecurityEvent;
 import name.abuchen.portfolio.model.SecurityEvent.DividendEvent;
+import name.abuchen.portfolio.model.Taxonomy;
 import name.abuchen.portfolio.model.Transaction.Unit;
 import name.abuchen.portfolio.money.Money;
 import name.abuchen.portfolio.money.Values;
@@ -51,6 +53,20 @@ public final class UpdateDividendsJob extends AbstractClientJob
         this.securities = new ArrayList<>(securities);
     }
 
+    private double getTaxRate(String ticker)
+    {
+        if(ticker.contains(".SW"))
+            return 0.35;
+        else if(ticker.contains(".PA"))
+            return 0.25;
+        else if(ticker.contains(".L") || ticker.contains(".HK"))
+            return 0;
+        else if(!ticker.contains(".") || ticker.contains(".AS"))
+            return 0.15;
+        else
+            return 0.3;
+    }
+    
     @Override
     protected IStatus run(IProgressMonitor monitor)
     {
@@ -64,8 +80,8 @@ public final class UpdateDividendsJob extends AbstractClientJob
         {
             try
             {
+                double taxRate = getTaxRate(security.getTickerSymbol());
                 List<DividendEvent> dividends = feed.getDividendPayments(security);
-
                 if (!dividends.isEmpty())
                 {
                     List<DividendEvent> current = security.getEvents().stream()
@@ -94,23 +110,57 @@ public final class UpdateDividendsJob extends AbstractClientJob
                                             && t.getDateTime().toLocalDate().compareTo(dividendEvent.getDate()) == 0
                                             && t.getOptionalSecurity().isPresent()
                                             && t.getOptionalSecurity().get().getTickerSymbol() == security.getTickerSymbol()).findFirst();
+                            
+                            long val = dividendEvent.getAmount().multiply(sharesAtDate).getAmount();
+     
+                            long tax = (long) (val * taxRate);
+                            Money calculatedTax = Money.of(security.getCurrencyCode(), val).multiplyAndRound(taxRate);
+                            
+                            
                             if(dividendTransaction.isEmpty() && sharesAtDate > 0)
                             {
                                 int ret = JOptionPane.showConfirmDialog(null, "New dividend found for " + security.getName() + " at date " + dividendEvent.getDate() + " would you like to import it ?");
                                 
                                 if(ret == JOptionPane.YES_OPTION)
                                 {                                
-                                    long val = dividendEvent.getAmount().multiply(sharesAtDate).getAmount();
-                                    long tax = (long) (val * 0.3);
                                     AccountTransaction at = new AccountTransaction(dividendEvent.getDate().atStartOfDay(), security.getCurrencyCode(), val - tax, security, AccountTransaction.Type.DIVIDENDS);
-                                    
-                                    Unit un = new Unit(Unit.Type.TAX, Money.of(security.getCurrencyCode(), tax));
+                                    Unit un = new Unit(Unit.Type.TAX, calculatedTax);
                                     at.addUnit(un);
                                     account.get().addTransaction(at);
                                     at.setShares(Values.Share.factorize(sharesAtDate));
                                     //at.setAmount(val);
                                 }
-                            }
+                            }/*
+                            else if(dividendTransaction.isPresent())
+                            {
+                                if(dividendTransaction.get().getAmount() != val - tax)
+                                {
+                                    int ret = JOptionPane.YES_OPTION; //JOptionPane.showConfirmDialog(null, "Dividend value difference for " + security.getName() + " at date " + dividendEvent.getDate() + " Diff: " + Money.of(security.getCurrencyCode(),(val - dividendTransaction.get().getAmount())) + " . Would you like to fix it ?");
+                                    
+                                    if(ret == JOptionPane.YES_OPTION)
+                                    {
+                                        dividendTransaction.get().setAmount(val - tax);
+                                    }
+                                }
+                                Optional<Unit> u = dividendTransaction.get().getUnit(Unit.Type.TAX);
+                                if(u.isPresent())
+                                {
+                                    Unit un = u.get();
+                                    int compareTxes = un.getAmount().compareTo(calculatedTax);
+                                    if(compareTxes != 0)
+                                    {
+                                        int ret = JOptionPane.YES_OPTION; //JOptionPane.showConfirmDialog(null, "Dividend tax difference for " + security.getName() + " at date " + dividendEvent.getDate() + " Diff: " + Money.of(security.getCurrencyCode(), compareTxes) + " . Would you like to fix it ?");
+                                        
+                                        if(ret == JOptionPane.YES_OPTION)
+                                        {
+                                            dividendTransaction.get().removeUnit(un);
+                                            Unit newUn = new Unit(Unit.Type.TAX, calculatedTax);
+                                            dividendTransaction.get().addUnit(newUn);
+                                            long t = 0;
+                                        }
+                                    }
+                                }
+                            }*/
                         }
                     }
 
